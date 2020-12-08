49274936a568c32afdcf2699c7f96382bbaff932
lewis <lewis.henderson@cobraflow.com>
2020-12-08 11:43:21 +0000

    Tidy up.

--- optaweb-vehicle-routing-backend/src/main/java/org/optaweb/vehiclerouting/plugin/security/SocketSecurityConfig.java
+++ optaweb-vehicle-routing-backend/src/main/java/org/optaweb/vehiclerouting/plugin/security/SocketSecurityConfig.java
@@ -1,11 +1,11 @@
 package org.optaweb.vehiclerouting.plugin.security;
 
+import static org.springframework.messaging.simp.SimpMessageType.*;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
 import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
-
-import static org.springframework.messaging.simp.SimpMessageType.*;
 
 @Configuration
 @Profile("keycloak")
